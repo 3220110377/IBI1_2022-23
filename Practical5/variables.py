e97546863af40d21ddd4e29b78800117604dc145
3220110377 <pinglei.22@intl.zju.edu.cn>
2023-03-15 18:48:49 +0800

    上传文件variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,18 @@
+a=-3.19
+b=-118.24
+c=116.39 #enter the values of a,b, and c
+d=min(abs(a-b),360-abs(a-b))
+e=min(abs(a-c),360-abs(a-c)) #enter the values of d and e, get the smaller of the two accuracy differences
+if d>e:
+ print('d>e')
+elif d==e:
+ print('d=e')
+else:
+ print('d<e') #compare d and e and output the results
+#d<e
+
+X,Y=True, False
+W=X and Y
+Z= X or Y
+print(W,Z)
+# W is False ,Z is True
