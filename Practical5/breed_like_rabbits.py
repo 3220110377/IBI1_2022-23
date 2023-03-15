87340563e8d7e73842a43215b870bfbaf0324798
3220110377 <pinglei.22@intl.zju.edu.cn>
2023-03-15 20:10:54 +0800

    Upload breed_like_rabbits.py

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,8 @@
+g=1 #generation
+n=2 #the number of rabbits or the number of water bottle rabbits need
+while n<100:
+ g=g+1
+ n=2*n #When the number of rabbits is less than 100, the number of generations is increased by one and the number of rabbits is doubled
+else:
+ print('When the age is %d, one hundred water bottles can be used up'%(g))
+#Output results, %d means a decimal integer output and use % () at the end to indicate the amount that should be output at %d
