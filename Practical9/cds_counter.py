5b2f739e393f56d62a48cd40b6ed15fa8fb23316
3220110377 <pinglei.22@intl.zju.edu.cn>
2023-04-17 18:03:20 +0800

    upload cds_counter.py

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -8,9 +8,9 @@ check the number of stop codons to get the number of possible coding sequences
 """
 import re
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'  # create a string variable seq
-if re.search('ATG', seq):
-    new = ''.join(map(str, re.findall(r'ATG.+', seq)))
-    print(len(re.findall(r'TAA|TAG|TGA', new)))
+if re.search('ATG', seq):  # make sure there is ATG sequence
+    new = ''.join(map(str, re.findall(r'ATG.+', seq)))  # view the sequence begins with ATG as new sequence
+    print(len(re.findall(r'TAA|TAG|TGA', new)))  # count the number of stop codons in the new sequence
 else:
-    print('no start codon')
+    print('no start codon')  # If there is no start codon, print no start codon
 
