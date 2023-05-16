77e55c02cdf2c2d7f773e7c381ba085aac70a38c
3220110377 <pinglei.22@intl.zju.edu.cn>
2023-05-16 21:43:21 +0800

    upload practical14

---
+++ Practical14/practical14.py
@@ -0,0 +1,25 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import pandas as pd
+data = {'id': [], 'name': [], 'definition': [], 'childNotes': []}
+dom = xml.dom.minidom.parse("go_obo.xml")
+root = dom.documentElement
+terms = root.getElementsByTagName("term")
+for term in terms:
+    id_node = term.getElementsByTagName("id")[0]
+    name_node = term.getElementsByTagName("name")[0]
+    defstr_node = term.getElementsByTagName("defstr")[0]
+    childNodes = 0
+    if "autophagosome" in defstr_node.firstChild.nodeValue:
+        for term_1 in terms:
+            is_a_nodes = term_1.getElementsByTagName("is_a")
+            for is_a_node in is_a_nodes:
+                is_a_value = is_a_node.firstChild.nodeValue
+                if id_node.firstChild.nodeValue == is_a_value:
+                    childNodes += 1
+        data['id'].append(id_node.firstChild.nodeValue)
+        data['name'].append(name_node.firstChild.nodeValue)
+        data['definition'].append(defstr_node.firstChild.nodeValue)
+        data['childNotes'].append(childNodes)
+df = pd.DataFrame(data)
+df.to_excel("Practical14.xlsx", index=False)
