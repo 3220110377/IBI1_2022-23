a9ae9248ffbed8984518bcc7c2fee9fec7eeea01
3220110377 <pinglei.22@intl.zju.edu.cn>
2023-05-17 13:34:14 +0800

    upload practical14

--- Practical14/practical14.py
+++ Practical14/practical14.py
@@ -1,6 +1,20 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import pandas as pd
+
+
+def childnodes(child_id):
+    number = 0
+    for term_child in terms:
+        id_node_child = term_child.getElementsByTagName("id")[0]
+        is_a_nodes = term_child.getElementsByTagName("is_a")
+        for is_a_node in is_a_nodes:
+            is_a_value = is_a_node.firstChild.nodeValue
+            if child_id == is_a_value:
+                number += childnodes(id_node_child.firstChild.nodeValue) + 1
+    return number  # A recursive algorithm is used to calculate the number of childNodes
+
+
 data = {'id': [], 'name': [], 'definition': [], 'childNotes': []}
 dom = xml.dom.minidom.parse("go_obo.xml")
 root = dom.documentElement
@@ -11,13 +25,7 @@ for term in terms:
     defstr_node = term.getElementsByTagName("defstr")[0]  # Store id, name and defstr
     childNodes = 0
     if "autophagosome" in defstr_node.firstChild.nodeValue:
-        for term_1 in terms:
-            is_a_nodes = term_1.getElementsByTagName("is_a")
-            for is_a_node in is_a_nodes:
-                is_a_value = is_a_node.firstChild.nodeValue
-                if id_node.firstChild.nodeValue == is_a_value:
-                    childNodes += 1  # Iterate through all <is_a>, if it matches the id,
-                    # then childNodes increment themselves by 1
+        childNodes = childnodes(id_node.firstChild.nodeValue)
         data['id'].append(id_node.firstChild.nodeValue)
         data['name'].append(name_node.firstChild.nodeValue)
         data['definition'].append(defstr_node.firstChild.nodeValue)
